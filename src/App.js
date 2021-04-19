bdbbc6f0ac625b0868e11f7fe653bcbcb2b68988
Alejandro Galeano <ale.gb32@gmail.com>
2021-04-18 22:16:12 -0300

    Express todo list con react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,73 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
 
+//Importar componentes
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
 function App() {
+  //states
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus ] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  //Run once
+  useEffect(()=>{
+    getLocalTodos();
+  },[])
+  //use effect
+  useEffect(()=>{
+    const filterHandler = () => {
+      switch (status) {
+        case 'completed':
+          setFilteredTodos(todos.filter(todo => todo.completed === true))        
+          break;
+        case 'uncompleted':
+          setFilteredTodos(todos.filter(todo => todo.completed === false))        
+          break;
+        default:
+          setFilteredTodos(todos);
+          break;
+      }
+    };
+    const saveLocalTodos = () => {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    };
+    filterHandler();
+    saveLocalTodos()
+  },[todos, status])
+  //functions 
+
+  
+
+  const getLocalTodos = () => {
+    if (localStorage.getItem("todos") === null) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem("todos"));
+      setTodos(todoLocal);
+    }
+  }
+  
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <header>
+        <h1>Lista de tareas de Ale</h1>
+      </header>
+      <Form
+        todos={todos}
+        filteredTodos={filteredTodos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        inputText={inputText}
+        setStatus={setStatus}
+      />
+      <TodoList
+        setTodos={setTodos}
+        todos={todos}
+        filteredTodos={filteredTodos}
+        setFilteredTodos={setFilteredTodos}
+      />
     </div>
   );
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,23 @@
+import React from "react";
+// importar componentes
+import Todo from "./Todo";
+
+const TodoList = ({ todos, setTodos, filteredTodos }) => {
+    return (
+        <div className="todo-container">
+            <ul className="todo-list">
+                {filteredTodos.map((todo)=>(
+                    <Todo
+                        text = { todo.text} 
+                        key = {todo.id}
+                        todo = {todo}
+                        setTodos = { setTodos }
+                        todos = { todos }
+                    />
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default TodoList;
